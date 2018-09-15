46a706ecdbde8c6dfa59a9017fd1be7523194b0b
Eric <9518999@qq.com>
2018-09-15 19:53:15 +0800

    员工档案，外出申请API

--- server/api/app/Http/Controllers/Admin/Staff/IndexController.php
+++ server/api/app/Http/Controllers/Admin/Staff/IndexController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Admin\Staff;
 
+use App\Http\Requests\Admin\Staff\CreateRequest;
+use App\Http\Requests\Admin\Staff\UpdateRequest;
 use App\Models\Staff;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -13,9 +15,15 @@ class IndexController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request, Staff $staff)
     {
-        //
+        $staff = $this->search($request, $staff);
+        return success_json($staff->paginate( config('pageinfo.per_page') ));
+    }
+
+    public function search(Request $request, Staff $staff)
+    {
+        return $staff;
     }
 
     /**
@@ -23,9 +31,22 @@ class IndexController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(CreateRequest $request, Staff $staff)
     {
-        //
+        $data = $request->only([
+            'name',
+        ]);
+        //$request['source'] = $request->get('source', 3);
+        $data['created_by'] = '新增';
+        $data['updated_by'] = '新增';
+
+        $ret = $staff->forceFill($data)->save();
+
+        if ($ret) {
+            return success_json($staff, '');
+        }
+
+        return error_json('新增失败，请检查');
     }
 
     /**
@@ -68,9 +89,19 @@ class IndexController extends Controller
      * @param  \App\Models\Staff  $staff
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Staff $staff)
+    public function update(UpdateRequest $request, Staff $staff)
     {
-        //
+        $data = $request->only([
+            'name',
+        ]);
+        $data['updated_by'] = '修改';
+        $ret = $staff->forceFill($data)->save();
+
+        if ($ret) {
+            return success_json($staff, '');
+        }
+
+        return error_json('修改失败，请检查');
     }
 
     /**
@@ -81,6 +112,7 @@ class IndexController extends Controller
      */
     public function destroy(Staff $staff)
     {
-        //
+        $staff->delete();
+        return success_json($staff, '删除成功');
     }
 }
