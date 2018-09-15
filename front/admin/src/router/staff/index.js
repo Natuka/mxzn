2b494ae28b46af6edf71dab1882cf3b7540d425a
natu si <929755313@qq.com>
2018-09-15 17:49:11 +0800

    新增路由

--- front/admin/src/router/staff/index.js
+++ front/admin/src/router/staff/index.js
@@ -5,16 +5,42 @@ export default [
     path: '/staff',
     name: 'staff',
     component: Main,
+    meta: {
+      access: ['staff_list'],
+      title: '客户管理',
+    },
+    // redirect: '/staff/doc',
     children: [
       {
         path: '/',
         name: 'staff_list',
         meta: {
           icon: '_qq',
-          title: '账号管理'
+          title: '人员档案',
+          access: ['staff_list']
         },
         component: () => import('@/view/staff/index.vue')
-      }
+      },
+      {
+        path: '/',
+        name: 'out_apply',
+        meta: {
+          icon: '_qq',
+          title: '外出申请',
+          access: ['staff_docs']
+        },
+        component: () => import('@/view/staff/index.vue')
+      },
+      {
+        path: '/',
+        name: 'contact',
+        meta: {
+          icon: '_qq',
+          title: '客户管理',
+          access: ['staff_contact']
+        },
+        component: () => import('@/view/staff/index.vue')
+      },
     ]
   }
 ]
