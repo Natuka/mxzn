ca3570845c75d6e0007bdc35a45cc2a411d9f147
Eric <9518999@qq.com>
2018-09-20 23:14:48 +0800

    员工API修改

--- server/api/app/Models/Document.php
+++ server/api/app/Models/Document.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Document extends Model
 {
     //
+    protected $table = 'base_documents';
+
 }
--- server/api/app/Models/Staff.php
+++ server/api/app/Models/Staff.php
@@ -7,4 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Staff extends Model
 {
     //
+    protected $table = 'base_staffs';
+
+    //员工编号，系统自动编号：日期年2位月2位日2位+流水码3位
+    public static function staffCode($date, $repeat = 3, $format = 'ymd')
+    {
+        $prefix = date($format, strtotime($date));
+        $last = static::where('number', 'like', $prefix . '%')->orderBy('id', 'desc')->first();
+
+        //第一个
+        if (!$last) {
+            return $prefix . sprintf('%0' . $repeat . 'd', 1);
+        }
+        return $last->number + 1;
+    }
 }
