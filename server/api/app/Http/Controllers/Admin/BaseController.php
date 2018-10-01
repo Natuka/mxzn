4b8080bbf640053a325b532ec74fb5e9bc45b430
natu si <929755313@qq.com>
2018-10-01 10:40:57 +0800

    新增公共类

---
+++ server/api/app/Http/Controllers/Admin/BaseController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Http\Controllers\Controller;
+
+class BaseController extends Controller
+{
+    /**
+     * @param Model $model
+     * @param null $customPage
+     * @return mixed
+     */
+    public function paginate(Model $model, $customPage = null)
+    {
+        return $model->paginate($customPage ? (int) $customPage : config('pageinfo.per_page'));
+    }
+}
