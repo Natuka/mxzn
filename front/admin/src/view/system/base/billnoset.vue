25b5744d7c6ed403434cdff630cdac45ce720c8c
youhaishen <canyn@sina.com>
2018-10-05 15:14:49 +0800

    system menu update

---
+++ front/admin/src/view/system/base/billnoset.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>系统管理 -> 基础设置 -> 单号设置</div>
+</template>
+<script>
+export default {
+  name: 'billnoset'
+}
+</script>
---
+++ front/admin/src/view/system/base/weixinset.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>系统管理 -> 基础设置 -> 微信设置</div>
+</template>
+<script>
+export default {
+  name: 'weixinset'
+}
+</script>
