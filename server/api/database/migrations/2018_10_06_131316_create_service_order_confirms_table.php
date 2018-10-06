26175362367d1514774e9657408ca32b742ed555
youhaishen <canyn@sina.com>
2018-10-06 22:04:34 +0800

    table add

--- server/api/database/migrations/2018_10_06_131316_create_service_order_confirms_table.php
+++ server/api/database/migrations/2018_10_06_131316_create_service_order_confirms_table.php
@@ -16,11 +16,17 @@ class CreateServiceOrderConfirmsTable extends Migration
         Schema::create('service_order_confirms', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('service_order_id')->nullable()->comment('order id');
-
-
-
+            $table->tinyInteger('is_solve')->nullable()->comment('是否解决: 1是,0否');
+            $table->tinyInteger('overall_satisfaction')->nullable()->comment('整体满意度,5星,10分制');
+            $table->tinyInteger('timeliness')->nullable()->comment('服务及时性,5星,10分制');
+            $table->tinyInteger('service_staff_atisfaction')->nullable()->comment('服务人员满意度,5星,10分制');
+            $table->tinyInteger('cost_performance')->nullable()->comment('性价比满意度,5星,10分制');
+            $table->integer('confirm_user_id')->nullable()->comment('确认用户id');
+            $table->string('confirm_user_name',20)->nullable()->comment('确认用户名字');
+            $table->string('opinions_suggestions',250)->nullable()->comment('意见与建议');
             $table->string('created_by',20)->nullable()->comment('建档人员');
             $table->string('updated_by',20)->nullable()->comment('修改人员');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
--- server/api/database/migrations/2018_10_06_131417_create_service_order_followups_table.php
+++ server/api/database/migrations/2018_10_06_131417_create_service_order_followups_table.php
@@ -16,11 +16,15 @@ class CreateServiceOrderFollowupsTable extends Migration
         Schema::create('service_order_followups', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('service_order_id')->nullable()->comment('order id');
-
-
-
+            $table->string('followup_staff',20)->nullable()->comment('催单人员');
+            $table->string('mobile',20)->nullable()->comment('手机');
+            $table->integer('handle_staff_id')->nullable()->comment('受理人员id');
+            $table->string('handle_staff_name',20)->nullable()->comment('受理人员');
+            $table->string('handle_result',20)->nullable()->comment('处理结果');
+            $table->string('remark',200)->nullable()->comment('备注');
             $table->string('created_by',20)->nullable()->comment('建档人员');
             $table->string('updated_by',20)->nullable()->comment('修改人员');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
