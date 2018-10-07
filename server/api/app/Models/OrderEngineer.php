8c353e07b7717f08b4b87a7a011b5959e4e9be93
natu si <929755313@qq.com>
2018-10-07 09:18:26 +0800

    修正员工&工程师获取

--- server/api/app/Models/OrderEngineer.php
+++ server/api/app/Models/OrderEngineer.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderEngineer extends Model
 {
+    protected $table = 'service_order_engineers';
     //
+    public function user()
+    {
+        return $this->morphOne(User::class, 'userable');
+    }
+
+    /**
+     * 员工
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function staff()
+    {
+        return $this->belongsTo(Staff::class, 'staff_id');
+    }
 }
