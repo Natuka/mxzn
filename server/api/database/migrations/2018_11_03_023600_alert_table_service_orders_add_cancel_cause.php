da21451d7da7832b981fb35be3aacf2463a5150a
Eric <9518999@qq.com>
2018-11-03 10:43:49 +0800

    修改工单主表

---
+++ server/api/database/migrations/2018_11_03_023600_alert_table_service_orders_add_cancel_cause.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlertTableServiceOrdersAddCancelCause extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('service_orders', function (Blueprint $table) {
+            //增加取消原因栏位
+            $table->tinyInteger('cancel_status')->length(1)->default(0)->notnull()->comment('取消状态1')->after('remark');
+            $table->string('cancel_cause', 600)->notnull()->default('')->comment('取消原因')->after('cancel_status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('service_orders', function (Blueprint $table) {
+            //
+        });
+    }
+}
