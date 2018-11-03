21b4be89c4f4476e79612c4a078d86b6a85abda4
natu si <929755313@qq.com>
2018-11-03 10:00:42 +0800

    新增用户以及当前日期

---
+++ server/wechat/resources/js/mixins/user.js
@@ -0,0 +1,12 @@
+import dayjs from "dayjs";
+
+export default {
+    computed: {
+        user () {
+            return this.$store.getters.user
+        },
+        currentDatetime () {
+            return dayjs().format('YYYY-MM-DD HH:ii:ss')
+        }
+    }
+}
