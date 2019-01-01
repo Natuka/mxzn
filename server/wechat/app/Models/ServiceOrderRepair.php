1efe3ab8c247fdf0453c9bbe818ec3646632fafb
natuka <929755313@qq.com>
2019-01-01 19:39:27 +0800

    新增处理过程附件

--- server/wechat/app/Models/ServiceOrderRepair.php
+++ server/wechat/app/Models/ServiceOrderRepair.php
@@ -7,4 +7,26 @@ use Illuminate\Database\Eloquent\Model;
 class ServiceOrderRepair extends Model
 {
     //
+
+    protected $appends = ['cause_docs', 'step_docs'];
+
+    public function getCauseDocsAttribute()
+    {
+        $ids = $this->attributes['cause_doc_ids'];
+        if (empty($ids)) {
+            return [];
+        }
+
+        return Document::find(explode(',', $ids));
+    }
+
+    public function getStepDocsAttribute()
+    {
+        $ids = $this->attributes['step_doc_ids'];
+        if (empty($ids)) {
+            return [];
+        }
+
+        return Document::find(explode(',', $ids));
+    }
 }
